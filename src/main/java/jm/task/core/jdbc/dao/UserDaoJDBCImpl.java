441538009be140786892e652b14092d248be18d5
Sarkis06 <sarkis.06@mail.ru>
2025-01-09 18:31:58 +0500

    Исправил ошибки

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection conn = Util.getConnection();
 
     public UserDaoJDBCImpl() {
     }
